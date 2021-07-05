30d3251b8d2f1d9135e76549688663479c498a8f
Sourav Mishra <joinsouravmishra@gmail.com>
2021-07-06 01:07:35 +0530

    sourav

--- server.js
+++ server.js
@@ -8,14 +8,33 @@ var bcrypt = require("bcrypt")
 var expressSession = require("express-session")
 var formidable = require("formidable")
 var fileSystem = require("fs")
-var {getVideoDurationInSeconds, default: getVideoDurationInSeconds} = require("get-video-duration")
+var jwt = require("jsonwebtoken")
+const excelToJson = require('convert-excel-to-json');
+const multer = require('multer');
+const fs = require('fs');
+const cron = require("node-cron");
+const { Server } = require("socket.io");
+const io = new Server(http);
+global.__basedir = __dirname;
+ 
+// -> Multer Upload Storage
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, __basedir + '/uploads/')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + "-" + Date.now() + "-" + file.originalname)
+    }
+});
+ 
+const upload = multer({storage: storage});
 
-app.use(expressSession({
-    "key":"user_id",
-    "secret":"User secret Object Id",
-    "resave":true,
-    "saveUninitialized":true
-}))
+// cron job scheduling
+cron.schedule("0 0 * * *", async function() {
+    console.log("running a task every midnight");
+   await addUserCountToDB()
+});
+
 
 // helper function to return user doc
 function getUser(id,callback){
@@ -24,6 +43,86 @@ function getUser(id,callback){
     }, function(err,user){
         callback(user)
     })
+}
+function isAuthenticated(req, res, next) {
+    if (typeof req.headers.authorization !== "undefined") {
+        // retrieve the authorization header and parse out the
+        // JWT using the split function
+        let token = req.headers.authorization.split(" ")[1];
+        // Here we validate that the JSON Web Token is valid and has been 
+        // created using the same private pass phrase
+        jwt.verify(token, "MySuperSecretPassPhrase", { algorithm: "HS256" }, (err, user) => {
+            
+            // if there has been an error...
+            if (err) {  
+                // shut them out!
+                res.status(500).json({ error: "Not Authorized" });
+                throw new Error("Not Authorized");
+            }
+            // if the JWT is valid, allow them to hit
+            // the intended endpoint
+            return next();
+        });
+    } else {
+        // No authorization header exists on the incoming
+        // request, return not authorized and throw a new error 
+        res.status(500).json({ error: "Not Authorized" });
+        throw new Error("Not Authorized");
+    }
+}
+async function addUserCountToDB(){
+   let result = await db.collection("users").find( { $where: function() { 
+        today = new Date(); //
+        today.setHours(0,0,0,0);
+        return (this._id.getTimestamp() >= today)
+    } } );
+
+    db.collection("userLogs").insertOne({
+        "count":result.length,
+        date:new Date()
+    }, (err, res) => {
+        if (err) throw err;
+        console.log("Number of documents inserted: " + res.insertedCount);
+        
+    });
+}
+// -> Import Excel File to MongoDB database
+function importExcelData2MongoDB(filePath){
+    // -> Read Excel File to Json Data
+    const excelData = excelToJson({
+        sourceFile: filePath,
+        sheets:[{
+            // Excel Sheet Name
+            name: 'Users',
+ 
+            // Header Row -> be skipped and will not be present at our result object.
+            header:{
+               rows: 1
+            },
+			
+            // Mapping columns to keys
+            columnToKey: {
+                A: '_id',
+                B: 'name',
+                C: 'address',
+                D: 'age'
+            }
+        }]
+    });
+ 
+    // -> Log Excel Data to Console
+    console.log(excelData);
+ 
+   
+
+    // Insert Json-Object to MongoDB
+    db.collection("users").insertMany(excelData.Users, (err, res) => {
+        if (err) throw err;
+        console.log("Number of documents inserted: " + res.insertedCount);
+        
+    });
+			
+    fs.unlinkSync(filePath);
 }
 app.use(express.json());
 app.use(express.urlencoded({
@@ -35,24 +134,16 @@ http.listen(3000,function(){
     console.log("server started on localhost:3000...!!");
 
     mongoClient.connect("mongodb://localhost/27017",{
+        
         useNewUrlParser: true,
         useUnifiedTopology: true
       }, function(err,client){
-        database = client.db("my_video_streaming")
+        database = client.db("node-mongo");
+        db.collection("users").createIndex( { name: "text", description: "text" } )
 
-        app.get("/", function(req,res){
-            res.render("index",{
-                "isLogIn": req.session.user_id?true:false
-            })
-        })
-    
-        app.get("/signup", function(req,res){
-            res.render("signup")
-        })
-    
-        
+       
 
-        app.post("/signup", function(req,res){
+        app.post("/signup", (req,res)=>{
             console.log('body', req)
             database.collection("users").findOne({
                 "email": req.body.email
@@ -61,47 +152,62 @@ http.listen(3000,function(){
                     bcrypt.hash(req.body.password,10, function(err,hash){
                         database.collection("users").insertOne({
                             "name": req.body.name,
+                            "about":req.body.about,
                             "email": req.body.email,
                             "password": hash,
-                            "coverPhoto":"",
-                            "image":"",
-                            "subscribers":"",
-                            "subscriptions":[],
-                            "playlists":[],
-                            "videos":[],
-                            "history":[],
-                            "notifications":[]
                         }, function(err,data){
-                            res.redirect("/login")
+                            db.collection("users").createIndex( { name: "text", description: "text" } )
+
+                            res.status(200).json({
+                                message:"User succesfully created..!"
+                            })
                         })
                     })
                 }else{
-                    res.send("Email already exist..!!")
+                    res.status(409).json({
+                        message:"User succesfully created..!"
+                    })
                 }
             })
         })
 
-        app.get("/login", function(req,res){
-            res.render("login", {
-                "error":"",
-                "message":""
-            })
-        })
+        
 
-        app.post("/login", function(req,res){
+        app.post("/login", (req,res)=>{
             
             database.collection("users").findOne({
                 "email": req.body.email
             }, function(err,user){
                 if(user===null){
-                    res.send("Email does not exist..!!")
+                    res.status(404).json({
+                        message:"User does not exsist..!"
+                    })
                 }else{
                     bcrypt.compare(req.body.password,user.password, function(err,iseVerified){
                         if(iseVerified){
-                            req.session.user_id = user._id;
-                            res.redirect("/")
+                            let token = jwt.sign({"id":user._id,"email":user.email}, "MySuperSecretPassPhrase", { algorithm: 'HS256'},{
+
+                                expiresIn: '365d' // expires in 365 days
+                     
+                           });
+
+                           io.on('connection', (socket) => {
+                            console.log('a user connected');
+                            socket.on('disconnect', () => {
+                              console.log('user disconnected');
+                            });
+                          });
+
+
+                            res.status(200).json({
+                                token:token,
+                                message:"User user logged in succefully..!"
+                            })
                         }else{
-                            res.send("Password is not correct..!!")
+                            res.status(401).json({
+                               
+                                message:"Password wrong..!"
+                            })
                         }
                     })
                     
@@ -109,99 +215,27 @@ http.listen(3000,function(){
             })
         })
 
-        app.get("/logout", function(req,res){
-            req.session.destroy();
-            res.redirect("/")
+       
+
+      
+
+        app.post('/api/uploadfile',isAuthenticated, upload.single("uploadfile"), (req, res) =>{
+            importExcelData2MongoDB(__basedir + '/uploads/' + req.file.filename);
+            res.status(200).json({
+                "message": 'File uploaded/import successfully!', 'file': req.file
+            });
+        });
+
+        app.get("/search", isAuthenticated, async(req,res)=>{
+           let result = await  db.stores.find( { $text: { $search: req.query.text} } )
+           res.status(200).json({
+            "message":"Your serach result..!!",
+            data:result,
+            count:result.length
+        });
         })
 
-        app.get("/upload", function(req,res){
-            if(req.session.user_id){
-                res.render("upload",{
-                    "islogIn": true
-                })
-            }else{
-                res.redirect("/login")
-            }
-        })
 
-        app.post("/upload-video", function(req,res){
-            if(req.session.user_id){
-            var formData = new formidable.IncomingForm();
-            formData.maxFileSIze = 1000 * 1024 * 1024;
-            formData.parse(req, function(error,fields,files){
-                var title = fields.title;
-                var description = fields.description;
-                var category = fields.category;
-                var tags = fields.tags
-
-                var oldPathThumbnail = files.thumbnail.path;
-                var thumbnail = "/public/thumbnails" + new Date().getTime() + '-' + files.thumbnail.name;
-
-                fileSystem.rename(oldPathThumbnail,thumbnail, function(err){
-
-                })
-
-                var oldPathVideo = files.video.path;
-                var newPath = "/public/videos/" + new Date().getTime() + '-' + files.video.name
-
-                fileSystem.rename(oldPathVideo,newPath, function(err){
-
-                    getUser(req.session.user_id, function(user){
-                        var currentTime = new Date().getTime();
-
-                        getVideoDurationInSeconds(newPath).then(function(duration){
-                            var hours = Math.floor(duration/60/60);
-                            var minutes = Math.floor(duration/60)- (hourts * 60);
-                            var seconds = Math.floor(duration%60);
-
-                            database.collection("videos").insertOne({
-                                "user":{
-                                    "id": user._id,
-                                    "id": user.name,
-                                    "id": user.image,
-                                    "subscribers": user.subscribers
-                                },
-                                "filePath":newPath,
-                                "thumbnail":thumbnail,
-                                "title":title,
-                                "description":description,
-                                "tags":tags,
-                                "category":category,
-                                "createdAt":currentTime,
-                                "hours":hours,
-                                "minutes":minutes,
-                                "seconds":seconds,
-                                "watch":currentTime,
-                                "viewa":0,
-                                "playlists":"",
-                                "likers":[],
-                                "dislikers":[],
-                                "comments":[]
-                                 },function (error,data) {
-                                   database.collection("users").updateOne({
-                                       "_id": ObjectId(req.session.user_id)
-                                   },{
-                                       $push:{
-                                           "videos":{
-                                               "_id":data.insertedId,
-                                               "thumbnail":thumbnail,
-                                               "title":title,
-                                               "watch":currentTime,
-                                               "viewa":0
-
-                                           }
-                                       }
-                                   })  
-                                 });
-                                 res.redirect("/");
-                        })
-                    })
-                })
-            })
-            }else{
-                res.redirect("/login")
-            }
-        })
     })
     
 })
